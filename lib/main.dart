113c932c35e3ab1add7fdae7b8997a96abe90251
Heba_Ramzii <hebaramzii33@gmail.com>
2025-01-07 22:43:35 +0200

    add

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled2/core/utils/colors_manager.dart';
 import 'package:untitled2/feature/view/login_screen.dart';
-import 'package:untitled2/feature/view/product_group_screen.dart';
 import 'feature/viewmodels/login/login_cubit.dart';
 import 'feature/viewmodels/product/product_cubit.dart';
 
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: ColorsManager.primaryColor),
         primaryColor: ColorsManager.primaryColor,
       ),
-      home:   ProductGroupScreen(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     ),
     );
